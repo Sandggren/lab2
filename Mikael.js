e8e029d53a3e3277046d13c20fa957ba7facf060
Sandggren <micke5801@gmail.com>
2024-10-31 20:27:21 +0100

    Update

--- Mikael.js
+++ Mikael.js
@@ -1,5 +1,14 @@
-const emailSubmit = document.getElementById("email")
-const firstNameSubmit = document.getElementById("firstName")
-const lastNameSubmit = document.getElementById("lastName")
-const submitForm = document.getElementById("submit")
+const email = document.getElementById("email")
+const firstName = document.getElementById("firstName")
+const lastName = document.getElementById("lastName")
+const quiz = document.getElementById("quiz")
 
+
+quiz.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    if (email.value == "") {
+        alert("Please write your email");
+    }
+
+});
