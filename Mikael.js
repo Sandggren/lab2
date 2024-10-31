fa97f2db692c258fb0c27a9a639cc160322fa41d
Sandggren <micke5801@gmail.com>
2024-10-31 01:49:10 +0100

    Added variables to js page and id's

---
+++ Mikael.js
@@ -0,0 +1,5 @@
+const emailSubmit = document.getElementById("email")
+const firstNameSubmit = document.getElementById("firstName")
+const lastNameSubmit = document.getElementById("lastName")
+const submitForm = document.getElementById("submit")
+
