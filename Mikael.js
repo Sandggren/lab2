ce67aa9c44f259193a7ccaa89e9e463469378e96
Sandggren <micke5801@gmail.com>
2024-11-05 14:22:14 +0100

    update

--- Mikael.js
+++ Mikael.js
@@ -2,13 +2,32 @@ const email = document.getElementById("email")
 const firstName = document.getElementById("firstName")
 const lastName = document.getElementById("lastName")
 const quiz = document.getElementById("quiz")
+const errors = document.getElementById('error')
 
 
 quiz.addEventListener("submit", function(event) {
-    event.preventDefault();
+    
+    let errorMessages = []
 
-    if (email.value == "") {
-        alert("Please write your email");
+    if (!email.value) {
+        errorMessages.push("Please write your email!");
+    }
+
+    if (!firstName.value) {
+        errorMessages.push("Please write your first name!");
+    }
+
+    if (!lastName.value) {
+        errorMessages.push("Please write your last name!");
+    }
+
+    if (errorMessages.length > 0) {
+        event.preventDefault();
+        errors.innerText = errorMessages.join(' ');
+    } 
+    
+    else {
+        alert("Quiz answers has been submitted successfully!");
     }
 
 });
